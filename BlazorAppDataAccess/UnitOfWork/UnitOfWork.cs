f258ff9b65639f83eff0a44287765d97d2586bde
Vladyslav Maksymenko <blakcwolf2017@gmail.com>
2023-07-13 14:06:31 +0300

    Implement IUnitOfWork.

---
+++ BlazorAppDataAccess/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using BlazorAppDataAccess.Interfaces;
+using BlazorAppDataAccess.Repository;
+using BlazorAppModels.Models;
+using BlazorAppwithIdentity.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorAppDataAccess.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+
+        public IRepository<Property> Properties { get; private set; }
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Properties = new Repository<Property>(_db);
+        }
+        public async Task SaveAsync()
+        {
+            await _db.SaveChangesAsync();
+        }
+    }
+}
