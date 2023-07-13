c36ca0dc3b9b64823a78a96e591774a5a3e34111
Vladyslav Maksymenko <blakcwolf2017@gmail.com>
2023-07-13 14:04:10 +0300

    Add interfaces IUnitOfWork and  IRepository.

---
+++ BlazorAppDataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorAppDataAccess.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetFirstOrDefaultAsync(Expression<Func<T, bool>> paramFilter, params string[] includeProp);
+        Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>>? paramFilter = null, params string[] includeProp);
+        Task AddAsync(T item);
+        Task RemoveAsync(int id);
+        Task UpdateAsync(T item);
+    }
+}
---
+++ BlazorAppDataAccess/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using BlazorAppModels.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorAppDataAccess.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IRepository<Property> Properties { get; }
+        Task SaveAsync();
+    }
+}
