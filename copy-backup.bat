91516e9668d9cb6a9709ac523db80c1d22682999
Vladyslav Maksymenko <blakcwolf2017@gmail.com>
2023-07-16 16:27:20 +0300

    Update bat

--- copy-backup.bat
+++ copy-backup.bat
@@ -1,5 +1,5 @@
-docker cp  /DbBack-up/TestBlazorDB3.bak sql-db:/var/opt/mssql/data/
+docker cp  ./DbBack-up/TestBlazorDB3.bak sql-db:/var/opt/mssql/data/
 
-docker cp  /DbBack-up/TestBlazorDB3.mdf sql-db:/var/opt/mssql/data/
+docker cp  ./DbBack-up/TestBlazorDB3.mdf sql-db:/var/opt/mssql/data/
 
-docker cp  /DbBack-up/TestBlazorDB3_log.ldf sql-db:/var/opt/mssql/data/
+docker cp  ./DbBack-up/TestBlazorDB3_log.ldf sql-db:/var/opt/mssql/data/
