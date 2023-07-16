e45c374dc5b651e867ee228d4fa8642bf05196e9
Vladyslav Maksymenko <blakcwolf2017@gmail.com>
2023-07-16 16:41:54 +0300

    Update file project

--- copy-backup.bat
+++ copy-backup.bat
@@ -1,5 +1,1 @@
-docker cp  ./DbBack-up/TestBlazorDB3.bak sql-db:/var/opt/mssql/data/
-
-docker cp  ./DbBack-up/TestBlazorDB3.mdf sql-db:/var/opt/mssql/data/
-
-docker cp  ./DbBack-up/TestBlazorDB3_log.ldf sql-db:/var/opt/mssql/data/
+docker cp ./DBBack-up/TestBlazorDB3.bak sql-db:/var/opt/mssql/data/
